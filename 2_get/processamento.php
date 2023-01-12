0f223572b0a7a22a181b032cad4a364ad8407fd8
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-12 11:45:00 -0300

    ok

--- 2_get/processamento.php
+++ 2_get/processamento.php
@@ -11,17 +11,19 @@
 </head>
 
 <body>
+    <div class="col-auto">
+        <form action="processa.php" method="post" enctype="multipart/form-data">
 
-    <form action="processa.php" method="post" enctype="multipart/form-data">
-
-        <label for="" class="form-label">Nome:</label>
-        <input type="text" name="nome" placeholder="Digite seu Nome" class="form-control">
-        <label for="" class="form-label">Idade:</label>
-        <input type="text" name="idade" placeholder="Digite sua Idade" class="form-control">
-        <label for="" class="form-label">Email:</label>
-        <input type="text" name="email" placeholder="Digite seu Email" class="form-control">
-        <button type="submit" name="submeter" class="btn btn-info">Enviar</button>
-    </form>
+            <label for="" class="form-label">Nome:</label>
+            <input type="text" name="nome" placeholder="Digite seu Nome" class="form-control form-control-sm">
+            <label for="" class="form-label">Idade:</label>
+            <input type="text" name="idade" placeholder="Digite sua Idade" class="form-control form-control-sm">
+            <label for="" class="form-label">Email:</label>
+            <input type="text" name="email" placeholder="Digite seu Email" class="form-control form-control-sm">
+            <br><br>
+            <button type="submit" name="submeter" class="btn btn-info">Enviar</button>
+        </form>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
