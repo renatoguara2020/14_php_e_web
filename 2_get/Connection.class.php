695403543a5a1b9a6b0d8d3a4aa8711313a2e9e1
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-12 21:54:28 -0300

    ok

---
+++ 2_get/Connection.class.php
@@ -0,0 +1,24 @@
+<?php
+
+
+ abstract Class  Connection {
+    
+private  $server = "mysql:host=localhost;dbname=cloudways";
+private  $user = "root";
+private  $pass = "";
+private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
+protected $conn;
+ 
+protected function Connection(){
+    try{
+          $this->conn = new PDO($this->server, $this->user,$this->pass,$this->options);
+          return $this->conn;
+        }catch (PDOException $e){
+            echo "There is some problem in connection: " . $e->getMessage();
+        }
+    }
+public function closeConnection() {
+     $this->conn = null;
+  }
+}
+?>
